5a7dbb1b6fb4c0cdaadb115a5986eed445a70246
dannyvassallo <danielvassallo87@gmail.com>
2015-06-09 21:29:14 -0400

    last steps

---
+++ client/tests/unit/helpers/body-class-test.js
@@ -0,0 +1,12 @@
+import {
+  bodyClass
+} from '../../../helpers/body-class';
+import { module, test } from 'qunit';
+
+module('BodyClassHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = bodyClass(42);
+  assert.ok(result);
+});
